7d1a9a8ede27a386b86c3d596adb5846abadec8d
Shei <flyinghuming.6000@gmail.com>
2025-11-01 15:24:55 +0600

    minor changes

--- Day 2/day2.rs
+++ Day 2/day2.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
-
 fn is_safe(levels: &Vec<i32>) -> bool {
     if levels.len() < 2 {
         return true;
@@ -13,48 +12,45 @@ fn is_safe(levels: &Vec<i32>) -> bool {
     for i in 1..levels.len() {
         let diff = levels[i] - levels[i - 1];
         let abs_diff = diff.abs();
-
         if abs_diff < 1 || abs_diff > 3 {
             return false;
         }
-
         if diff > 0 {
             decreasing = false;
         }
         if diff < 0 {
             increasing = false;
         }
-        
     }
     return increasing || decreasing;
 }
 
 fn main() -> io::Result<()> {
-        let path = "input.txt";
-    
-        let file = File::open(path)?;
-        let reader = io::BufReader::new(file);
-    
-        let mut safe_count = 0;
-    
-        for line in reader.lines() {
-            let line = line?;
-            if line.trim().is_empty() {
-                continue;
-            }
-    
-            // Parse numbers into a Vec<i32>
-            let levels: Vec<i32> = line
-                .split_whitespace()
-                .filter_map(|s| s.parse::<i32>().ok())
-                .collect();
-    
-            if is_safe(&levels) {
-                safe_count += 1;
-            }
+    let path = "input.txt";
+
+    let file = File::open(path)?;
+    let reader = io::BufReader::new(file);
+
+    let mut safe_count = 0;
+
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.trim().is_empty() {
+            continue;
         }
-    
-        println!("Safe reports: {}", safe_count);
-    
-        Ok(())
+
+        let levels: Vec<i32> = line
+            .split_whitespace()
+            .filter_map(|s| s.parse::<i32>().ok())
+            .collect();
+
+        if is_safe(&levels) {
+            safe_count += 1;
+        }
     }
+
+    println!("Safe reports: {}", safe_count);
+    Ok(())
+}
+    
